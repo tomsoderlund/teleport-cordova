402cdc4bff1ceae6269c4ed192fc332bf20b755f
Tom Söderlund <tom@tomsoderlund.com>
2016-12-30 11:55:33 +0100

    Merge 2 controllers into VideoStreamController

--- www/js/projectrtc.js
+++ www/js/projectrtc.js
@@ -1,8 +1,9 @@
 var ProjectRtcClient = (function() {
 
+	var PROJECTRTC_SERVER = 'https://projectrtc-tom.herokuapp.com/';
+
 	var app = angular.module('projectRtc', [],
 		function ($locationProvider) {
-			console.log('ProjectRtcClient running', this);
 			$locationProvider.html5Mode({ enabled: true, requireBase: false });
 		}
 		);
@@ -49,19 +50,19 @@ var ProjectRtcClient = (function() {
 		return camera;
 	}]);
 
-	app.controller('RemoteStreamsController', ['camera', '$location', '$http', function(camera, $location, $http) {
-		var rtc = this;
-		rtc.remoteStreams = [];
+	app.controller('VideoStreamController', ['camera', '$location', '$http', '$scope', '$window', function (camera, $location, $http, $scope, $window) {
+		var vtc = this;
+		vtc.remoteStreams = [];
 
 		function getStreamById(id) {
-			for (var i=0; i<rtc.remoteStreams.length;i++) {
-				if (rtc.remoteStreams[i].id === id) {return rtc.remoteStreams[i];}
+			for (var i=0; i<vtc.remoteStreams.length;i++) {
+				if (vtc.remoteStreams[i].id === id) {return vtc.remoteStreams[i];}
 			}
 		}
 
-		rtc.getActiveStreams = function () {
+		vtc.getActiveStreams = function () {
 			// get list of streams from the server
-			$http.get('https://projectrtc-tom.herokuapp.com/streams.json').then(function(results) {
+			$http.get(PROJECTRTC_SERVER + 'streams.json').then(function(results) {
 				// filter own stream
 				var streams = results.data.filter(function(stream) {
 					return stream.id != client.getId();
@@ -72,90 +73,143 @@ var ProjectRtcClient = (function() {
 			    	streams[i].isPlaying = (!!stream) ? stream.isPlaying : false;
 			    }
 			    // save new streams
-			    rtc.remoteStreams = streams;
+			    vtc.remoteStreams = streams;
 			}, function (httpErr) {
 				console.error('httpErr', httpErr);
 			});
 		};
 
-		rtc.view = function(stream) {
+		vtc.view = function(stream) {
 			client.peerInit(stream.id);
 			stream.isPlaying = !stream.isPlaying;
 		};
 
-		rtc.call = function(stream) {
+		var connectToStream = function (stream) {
+			client.toggleLocalStream(stream.id);
+			if (stream.isPlaying) {
+				client.peerRenegociate(stream.id);
+			}
+			else {
+				client.peerInit(stream.id);
+			}
+			stream.isPlaying = !stream.isPlaying;
+		};
+
+		vtc.call = function (stream) {
 			/* If json isn't loaded yet, construct a new stream 
 			 * This happens when you load <serverUrl>/<socketId> : 
 			 * it calls socketId immediatly.
 			 **/
-			 if(!stream.id) {
-			 	stream = {id: stream, isPlaying: false};
-			 	rtc.remoteStreams.push(stream);
-			 }
-			 if(camera.isOn) {
-			 	client.toggleLocalStream(stream.id);
-			 	if(stream.isPlaying) {
-			 		client.peerRenegociate(stream.id);
-			 	} else {
-			 		client.peerInit(stream.id);
-			 	}
-			 	stream.isPlaying = !stream.isPlaying;
-			 } else {
-			 	camera.start()
-			 	.then(function(result) {
-			 		client.toggleLocalStream(stream.id);
-			 		if(stream.isPlaying) {
-			 			client.peerRenegociate(stream.id);
-			 		} else {
-			 			client.peerInit(stream.id);
-			 		}
-			 		stream.isPlaying = !stream.isPlaying;
-			 	})
-			 	.catch(function(err) {
-			 		console.log(err);
-			 	});
-			 }
-			};
-
-		//initial load
-		rtc.getActiveStreams();
-		if($location.url() != '/') {
-			rtc.call($location.url().slice(1));
-		};
-	}]);
-
-	app.controller('LocalStreamController',['camera', '$scope', '$window', function(camera, $scope, $window) {
-		var localStream = this;
-		localStream.name = 'Guest';
-		localStream.link = '';
-		localStream.cameraIsOn = false;
-
-		$scope.$on('cameraIsOn', function(event,data) {
-			$scope.$apply(function() {
-				localStream.cameraIsOn = data;
-			});
-		});
-
-		localStream.toggleCam = function() {
-			if(localStream.cameraIsOn) {
-				camera.stop()
-				.then(function(result) {
-					client.send('leave');
-					client.setLocalStream(null);
-				})
-				.catch(function(err) {
-					console.log(err);
-				});
-			} else {
+			if (!stream.id) {
+				stream = { id: stream, isPlaying: false };
+				vtc.remoteStreams.push(stream);
+			}
+			if (camera.isOn) {
+				connectToStream(stream);
+			}
+			else {
 				camera.start()
-				.then(function(result) {
-					localStream.link = $window.location.host + '/' + client.getId();
-					client.send('readyToStream', { name: localStream.name });
+				.then(function (result) {
+					connectToStream(stream);
 				})
-				.catch(function(err) {
+				.catch(function (err) {
 					console.log(err);
 				});
 			}
 		};
+
+		//initial load
+		vtc.getActiveStreams();
+		if($location.url() != '/') {
+			vtc.call($location.url().slice(1));
+		};
+
+		vtc.name = 'Guest';
+		vtc.link = '';
+		vtc.cameraIsOn = false;
+
+		$scope.$on('cameraIsOn', function(event,data) {
+			$scope.$apply(function() {
+				vtc.cameraIsOn = data;
+			});
+		});
+
+		var startLocalStream = function () {
+			camera.start()
+			.then(function(result) {
+				vtc.link = PROJECTRTC_SERVER + client.getId();
+				client.send('readyToStream', { name: vtc.name });
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+		};
+
+		var stopLocalStream = function () {
+			camera.stop()
+			.then(function(result) {
+				client.send('leave');
+				client.setLocalStream(null);
+			})
+			.catch(function(err) {
+				console.log(err);
+			});			
+		};
+
+		vtc.toggleCam = function() {
+			if (vtc.cameraIsOn) {
+				stopLocalStream();
+			}
+			else {
+				startLocalStream();
+			}
+		};
+
+		vtc.getFirstStream = function () {
+			return vtc.remoteStreams[0];
+		};
+
+		vtc.getToggleCallReference = function () {
+			if (vtc.cameraIsOn) {
+				// Call in progress
+				return 'call-hangup';
+			}
+			else if (vtc.remoteStreams.length > 0) {
+				// No call in progress - there are existing calls on server
+				return 'call-join';
+			}
+			else {
+				// No call in progress - there are no existing calls on server
+				return 'call-startnew';
+			}
+		};
+
+		vtc.getToggleCallLabel = function () {
+			switch (vtc.getToggleCallReference()) {
+				case 'call-hangup':
+					return 'Hang up';
+				case 'call-join':
+					return 'Join call';
+				case 'call-startnew':
+					return 'Start new call';
+			};
+		};
+
+		vtc.toggleCall = function () {
+			switch (vtc.getToggleCallReference()) {
+				case 'call-hangup':
+					stopLocalStream();
+					break;
+				case 'call-join':
+					vtc.call(vtc.getFirstStream());
+					break;
+				case 'call-startnew':
+					startLocalStream();
+					break;
+			};
+			vtc.getActiveStreams();
+		};
+
 	}]);
+
 });
